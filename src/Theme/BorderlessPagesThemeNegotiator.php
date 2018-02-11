63090d89f04d865cfc8cf241daadd459f1d01004
aaron <aaron@neweden.co>
2018-02-11 16:22:06 +0000

    Implemented Theme Negotiator

---
+++ src/Theme/BorderlessPagesThemeNegotiator.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\borderless_grid_pages\Theme;
+
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Theme\ThemeNegotiatorInterface;
+use Drupal\node\Entity\Node;
+use Drupal\node\Entity\NodeType;
+use Drupal\node\NodeTypeInterface;
+
+/**
+* Set the theme on Borderless Pages
+*/
+class BorderlessPagesThemeNegotiator implements ThemeNegotiatorInterface {
+
+    /**
+     * Determine if the current page should be rendered using the Borderless Theme
+     *
+     * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+     *   The current route match object.
+     *
+     * @return bool
+     *   TRUE if the Borderless Theme is to be used, otherwise FALSE
+     */
+    public function applies(RouteMatchInterface $route_match) {
+        $node = $route_match->getParameter('node');
+
+        // return false if the current page is not a node
+        if ($node == null || !($node instanceof Node))
+            return false;
+
+        $type = NodeType::load($node->getType());
+        return $type->getThirdPartySetting('borderless_grid_pages', 'is_borderless_page', 0);
+    }
+
+    /**
+     * Returns the name of the Borderless Theme
+     *
+     * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+     *   The current route match object.
+     *
+     * @return string|null
+     *   The name of the Borderless Theme or NULL if the module config happens to not contain a value
+     */
+    public function determineActiveTheme(RouteMatchInterface $route_match) {
+        return \Drupal::config('borderless_grid_pages.settings')->get('theme_machine_name');
+    }
+
+}
+
